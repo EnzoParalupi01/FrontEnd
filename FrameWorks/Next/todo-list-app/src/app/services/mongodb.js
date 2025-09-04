fa9145a9dc1754416ddc6bf5c52722a3940db1cb
EnzoParalupi01 <enzoparalupi07@gmail.com>
2025-09-04 16:58:20 -0300

    04.09.2025

---
+++ FrameWorks/Next/todo-list-app/src/app/services/mongodb.js
@@ -0,0 +1,10 @@
+//arrow function
+
+import mongoose from "mongoose"
+
+const connectMongo = async () => {
+    mongoose.connect(process.env.DATABASE_URL) //estabelece conexão com o BD
+        .then(()=>console.log("Conectado ao MongoDB"));
+}
+
+export default connectMongo;
